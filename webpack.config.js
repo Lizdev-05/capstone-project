799372f111ebecd76208ab48a72542953a028230
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-25 20:05:10 +0100

    update the web-pack

--- webpack.config.js
+++ webpack.config.js
@@ -19,17 +19,13 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: './',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
       },
     ],
   },
