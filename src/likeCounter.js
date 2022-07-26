6ac04d3924b1ae1f3a530c4c101a3da82f9cbc47
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-26 22:29:48 +0100

    post the like url

--- src/likeCounter.js
+++ src/likeCounter.js
@@ -1,14 +1,21 @@
-// const URL_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/cZrT4b0n4BnZGphjyULr/likes';
+const URL_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/cZrT4b0n4BnZGphjyULr/likes';
 const mealContainer = document.querySelector('.meal-cards');
 
 const likes = () => {
-  console.log(likes);
   mealContainer.addEventListener('click', (e) => {
     const clicked = e.target.closest('.like-icon');
-    console.log(clicked);
     if (!clicked) return;
     const item = clicked.getAttribute('id');
-    console.log(item);
+    fetch(URL_API, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: item,
+      }),
+    });
   });
 };
 
