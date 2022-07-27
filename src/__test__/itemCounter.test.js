11df4eaec42d062b5957e0ecb5898af0c3119d15
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-27 21:50:51 +0100

    update responsiveness

--- src/__test__/itemCounter.test.js
+++ src/__test__/itemCounter.test.js
@@ -2,15 +2,19 @@
 const fetchMock = require('fetch-mock');
 
 describe('async fetch', () => {
-  it('can fetch', async () => {
+  test('can fetch', async () => {
     fetchMock.get('https://www.themealdb.com/api/json/v1/1/filter.php?a=British', {
       meals: [{ idMeal: '123' },
         { idMeal: '123' },
         { idMeal: '123' },
-        { idMeal: '123' }],
+        { idMeal: '123' },
+        { idMeal: '123' },
+        { idMeal: '123' },
+        { idMeal: '123' },
+      ],
     });
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=British');
     const data = await response.json();
-    expect(data.meals.length).toBe(4);
+    expect(data.meals.length).toBe(7);
   });
 });
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './assets/images/meal1.jpg';
 import './assets/images/meal2.jpg';
 import './assets/images/meal3.jpg';
 import './assets/images/meal4.jpg';
+import './assets/images/meal5.jpg';
 import './assets/images/close.svg';
 
 display();
